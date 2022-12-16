a781c62b2a415ce2e6f6f084ef0d906cb373459c
Carlos Patino <guillermo.patino@uabc.edu.mx>
2022-12-15 19:40:39 -0800

    commit

--- lib/app/data/services/dialog_service.dart
+++ lib/app/data/services/dialog_service.dart
@@ -12,7 +12,7 @@ class DialogService extends GetxService {
       colorText: color,
       snackPosition: SnackPosition.BOTTOM,
       isDismissible: true,
-      dismissDirection: SnackDismissDirection.HORIZONTAL,
+      dismissDirection:DismissDirection.horizontal,
       forwardAnimationCurve: Curves.easeOutBack,
       margin: const EdgeInsets.all(15),
     );
